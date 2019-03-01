ee807d08511232c0e5c3716f9b6d2798cb47959f
rimim <noemail@thanks.com>
2019-03-01 07:17:20 -0500

    Added documentation and made members private

--- src/i2c/I2CReceiver.h
+++ src/i2c/I2CReceiver.h
@@ -2,77 +2,115 @@
 #define I2CReceiver_h
 
 #include "ReelTwo.h"
+#include "core/AnimatedEvent.h"
 #include "core/CommandEvent.h"
 #include <Wire.h>
 
-#define USE_I2C
-#define SUBCLASS_I2C public I2CEvent
+/**
+  * \ingroup i2c
+  *
+  * \class I2CReceiverBase
+  *
+  * \brief Base template of automatic forwarder from i2c to CommandEvent
+  *
+  * Create an instance of this template to automatically forward i2c string commands to CommandEvent.
+  * A convenience type of I2CReceiver is provided that uses the default buffer size of 32 bytes. Only
+  * a single instance of I2CReceiver should be created per sketch.
+  *
+  * \code
+  * #include "i2c/I2CReceiver.h"
+  *
+  * I2CReceiver i2cReceiver(0x19);
+  * \endcode
+  *
+  * To create a receiver with a buffer size of 42 (for example) use:
+  *
+  * \code
+  * I2CReceiverBase<42> i2cReceiver(0x19);
+  * \endcode
+  *
+  */
+template<int bufferSize = 32>
+class I2CReceiverBase : public AnimatedEvent
+{
+public:
+    /** \brief Constructor
+      *
+      * Only a single instance of I2CReceiverBase should be created per sketch.
+      *
+      * \param i2caddress i2c address of this controller
+      */
+    I2CReceiverBase(byte i2caddress = 0x19)
+    {
+        *myself() = this;
+        Wire.begin(i2caddress);                  // Connects to I2C Bus and establishes address.
+        Wire.onReceive(i2cEvent);                // Register event so when we receive something we jump to i2cEvent();
+    }
+
+    /**
+      * Dispatch any received i2c event to CommandEvent
+      */
+    virtual void animate() override
+    {
+        if (fCmdReady && *fCmdString != 0)
+        {
+            CommandEvent::process(fCmdString);
+        }
+        fCmdReady = false;
+    }
+
+private:
+    char fCmdString[bufferSize];
+    bool fCmdReady = false;
+
+    void handleEvent(int howMany)
+    {
+        /* ignore any new i2c event until previous one has been processed */
+        if (fCmdReady)
+            return;
+        char* b = fCmdString;
+        for (byte i = 0; Wire.available();)
+        {
+            char ch = (char)Wire.read();
+            // Dont add leading whitespace
+            if (i < sizeof(fCmdString) - 1 && (i != 0 || !isspace(ch)))
+            {
+                fCmdString[i++] = ch;
+                fCmdString[i] = 0;
+            }
+        }
+        fCmdReady = true;
+    }
+
+    static void i2cEvent(int howMany)
+    {
+        (*myself())->handleEvent(howMany);
+    }
+
+    static I2CReceiverBase<bufferSize>** myself()
+    {
+        static I2CReceiverBase<bufferSize>* self;
+        return &self;
+    }
+
+    static constexpr bool sCreated = false;
+};
 
 /**
   * \ingroup i2c
   *
   * \class I2CReceiver
   *
-  * \brief Automatic forwarder from i2c to CommandEvent
+  * \brief Default instantiation of automatic forwarder from i2c to CommandEvent
   *
-  * Create an instance of this template to automatically forward i2c string commands to CommandEvent 
+  * Default instantiation of I2CReceiverBase with a buffer size of 32 bytes.
   *
   * \code
-  * I2CReceiver<> i2cReceiver(0x19);
+  * #include "i2c/I2CReceiver.h"
+  *
+  * I2CReceiver i2cReceiver(0x19);
   * \endcode
+  *
   */
-template<int bufferSize = 32>
-class I2CReceiver : public AnimatedEvent
-{
-public:
-	I2CReceiver(byte i2caddress = 0x19)
-	{
-		*myself() = this;
-		Wire.begin(i2caddress);                  // Connects to I2C Bus and establishes address.
-		Wire.onReceive(i2cEvent);                // Register event so when we receive something we jump to i2cEvent();
-	}
-
-	virtual void animate() override
-	{
-		if (fCmdReady && *fCmdString != 0)
-		{
-			CommandEvent::process(fCmdString);
-		}
-		fCmdReady = false;
-	}
-
-	void handleEvent(int howMany)
-	{
-		/* ignore any new i2c event until previous one has been processed */
-		if (fCmdReady)
-			return;
-		char* b = fCmdString;
-		for (byte i = 0; Wire.available();)
-		{
-			char ch = (char)Wire.read();
-			// Dont add leading whitespace
-			if (i < sizeof(fCmdString) - 1 && (i != 0 || !isspace(ch)))
-			{
-				fCmdString[i++] = ch;
-				fCmdString[i] = 0;
-			}
-		}
-		fCmdReady = true;
-	}
-
-	static void i2cEvent(int howMany)
-	{
-		(*myself())->handleEvent(howMany);
-	}
-
-private:
-	char fCmdString[bufferSize];
-	bool fCmdReady = false;
-
-	static I2CReceiver<bufferSize>** myself()
-	{
-		static I2CReceiver<bufferSize>* self;
-		return &self;
-	}
-};
+typedef I2CReceiverBase<> I2CReceiver;
 #endif
