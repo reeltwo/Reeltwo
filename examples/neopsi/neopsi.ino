0c60cc51759eaf5e3a5c8106dad2c866fe9248f1
rimim <nomail@thanks.com>
2022-08-06 06:45:57 -0400

    Added missing include

--- examples/neopsi/neopsi.ino
+++ examples/neopsi/neopsi.ino
@@ -1,6 +1,7 @@
 #include "ReelTwo.h"
 #include "dome/Logics.h"
 #include "dome/LogicEngineController.h"
+#include "dome/NeoPSI.h"
 
 AstroPixelFrontPSI<> frontPSI(LogicEngineFrontPSIDefault, 4);
 AstroPixelRearPSI<> rearPSI(LogicEngineRearPSIDefault, 5);
