891b5423e4970220c8a6c71b4c133ba61abde6e2
rimim <nomail@thanks.com>
2022-09-30 05:43:48 -0400

    Changed PWM decoder to use the Arduino esp32-hal instead of controlling the rmt driver directly

--- src/encoder/PWMDecoder.cpp
+++ src/encoder/PWMDecoder.cpp
@@ -1,50 +1,37 @@
 #if defined(ESP32)
 
-// IRAM_ATTR code in a header file causes compiler errors
-
 #include "PWMDecoder.h"
+
+// Check if we are compiling against a more recent hal layer
+#ifndef RMT_RX_MODE
+// Nope. arduino-esp32 1.0.6 will crash so we configure the RMT device manually instead
 #include "driver/rmt.h"
 #include "soc/rmt_reg.h"
-
-PWMDecoder* PWMDecoder::sActive;
 
 void IRAM_ATTR
 PWMDecoder::rmt_isr_handler(void* arg)
 {
-	// with reference to https://www.esp32.com/viewtopic.php?t=7116#p32383
-	// but modified so that this ISR only checks chX_rx_end
+	PWMDecoder* decoder = (PWMDecoder*)arg;
 	uint32_t intr_st = RMT.int_st.val;
 
-	// see declaration of RMT.int_st:
-	// takes the form of 
-	// bit 0: ch0_tx_end
-	// bit 1: ch0_rx_end
-	// bit 2: ch0_err
-	// bit 3: ch1_tx_end
-	// bit 4: ch1_rx_end
-	// ...
-	// thus, check whether bit (channel*3 + 1) is set to identify
-	// whether that channel has changed
-
-	for (unsigned i = 0; i < sActive->fNumChannels; i++)
+	uint8_t numChannels = decoder->fNumChannels;
+	for (unsigned i = 0; i < numChannels; i++)
 	{
-		uint8_t channel = sActive->fChannels[i];
-		uint32_t channel_mask = BIT(channel*3+1);
-
+		uint32_t channel_mask = BIT(i*3+1);
 		if (!(intr_st & channel_mask))
 			continue;
 
-		RMT.conf_ch[channel].conf1.rx_en = 0;
-		RMT.conf_ch[channel].conf1.mem_owner = RMT_MEM_OWNER_TX;
-		volatile rmt_item32_t* item = RMTMEM.chan[channel].data32;
+		RMT.conf_ch[i].conf1.rx_en = 0;
+		RMT.conf_ch[i].conf1.mem_owner = RMT_MEM_OWNER_TX;
+		volatile rmt_item32_t* item = RMTMEM.chan[i].data32;
 		if (item)
 		{
-			sActive->fRawPulse[i] = item->duration0;
+			decoder->fChannel[i].fRawPulse = item->duration0;
 		}
 
-		RMT.conf_ch[channel].conf1.mem_wr_rst = 1;
-		RMT.conf_ch[channel].conf1.mem_owner = RMT_MEM_OWNER_RX;
-		RMT.conf_ch[channel].conf1.rx_en = 1;
+		RMT.conf_ch[i].conf1.mem_wr_rst = 1;
+		RMT.conf_ch[i].conf1.mem_owner = RMT_MEM_OWNER_RX;
+		RMT.conf_ch[i].conf1.rx_en = 1;
 
 		//clear RMT interrupt status.
 		RMT.int_clr.val = channel_mask;
@@ -55,28 +42,29 @@ void
 PWMDecoder::checkActive(unsigned i)
 {
     uint32_t status = 0;
-    rmt_get_status((rmt_channel_t)fChannels[i], &status);
-    uint8_t state = (status & RMT_STATE_CH0) >> RMT_STATE_CH0_S;
-    if (state == 3)
+    if (i < fNumChannels)
     {
-		fLastActive[i] = millis();
-    }
+	    rmt_get_status(rmt_channel_t(i), &status);
+	    uint8_t state = (status & RMT_STATE_CH0) >> RMT_STATE_CH0_S;
+	    if (state == 3)
+	    {
+			fChannel[i].fLastActive = millis();
+	    }
+	}
 }
 
 void
 PWMDecoder::begin(void)
 {
-	if (sActive != nullptr)
+	if (fISRHandle != nullptr)
 		return;
-	sActive = this;
 	rmt_config_t rmt_channels[fMaxChannels] = {};
 	for (unsigned i = 0; i < fNumChannels; i++)
 	{
-		fPulse[i] = RECEIVER_CH_CENTER;
-		fRawPulse[i] = RECEIVER_CH_CENTER;
+		fChannel[i].fRawPulse = fChannel[i].fPulse = 1500;
 
-		rmt_channels[i].channel = (rmt_channel_t)fChannels[i];
-		rmt_channels[i].gpio_num = (gpio_num_t)fGPIO[i];
+		rmt_channels[i].channel = rmt_channel_t(i);
+		rmt_channels[i].gpio_num = gpio_num_t(fChannel[i].fGPIO);
 		rmt_channels[i].clk_div = 80;
 		rmt_channels[i].mem_block_num = 4;
 		rmt_channels[i].rmt_mode = RMT_MODE_RX;
@@ -89,26 +77,24 @@ PWMDecoder::begin(void)
 		rmt_rx_start(rmt_channels[i].channel, 1);
 	}
 
-	rmt_isr_register(rmt_isr_handler, NULL, 0, &fISRHandle);
+	rmt_isr_register(rmt_isr_handler, this, 0, &fISRHandle);
 	ESP_LOGI(TAG, "Init ISR on %d", xPortGetCoreID());
 }
 
 void
 PWMDecoder::end(void)
 {
-	if (sActive == this)
+	if (fISRHandle == nullptr)
+		return;
+	for (unsigned i = 0; i < fNumChannels; i++)
 	{
-		for (unsigned i = 0; i < fNumChannels; i++)
-		{
-			rmt_tx_stop((rmt_channel_t)fChannels[i]);
-		}
-		if (fISRHandle != nullptr)
-		{
-			rmt_isr_deregister(fISRHandle);
-			fISRHandle = nullptr;
-		}
-		sActive = nullptr;
+		rmt_tx_stop(rmt_channel_t(i));
+	}
+	if (fISRHandle != nullptr)
+	{
+		rmt_isr_deregister(fISRHandle);
+		fISRHandle = nullptr;
 	}
 }
-
+#endif
 #endif
--- src/encoder/PWMDecoder.h
+++ src/encoder/PWMDecoder.h
@@ -9,23 +9,22 @@
 #include "core/SetupEvent.h"
 #include "core/AnimatedEvent.h"
 #include "driver/rmt.h"
+#include "esp32-hal.h"
 
-#define RECEIVER_CH_MIN 1000
-#define RECEIVER_CH_CENTER 1500
-#define RECEIVER_CH_MAX 2000
+#ifndef RMT_RX_MODE
+#define RMT_RX_MODE false
+#endif
 
-class PWMDecoder: public SetupEvent, AnimatedEvent
+class PWMDecoder: public AnimatedEvent
 {
 public:
 	PWMDecoder(void (*changeNotify)(int pin, uint16_t pwm), int pin1, int pin2 = -1, int pin3 = -1, int pin4 = -1) :
 		fChangeNotify(changeNotify)
 	{
-		for (unsigned i = 0; i < fMaxChannels; i++)
-			fChannels[i] = i+1;
-		fGPIO[0] = uint8_t(pin1);
-		fGPIO[1] = uint8_t(pin2);
-		fGPIO[2] = uint8_t(pin3);
-		fGPIO[3] = uint8_t(pin4);
+		fChannel[0].fGPIO = uint8_t(pin1);
+		fChannel[1].fGPIO = uint8_t(pin2);
+		fChannel[2].fGPIO = uint8_t(pin3);
+		fChannel[3].fGPIO = uint8_t(pin4);
 		fNumChannels = 4;
 		if (pin4 == -1)
 			fNumChannels = 3;
@@ -42,17 +41,17 @@ public:
 
 	inline uint16_t channel(unsigned i = 0) const
 	{
-		return fRawPulse[i];
+		return fChannel[i].fRawPulse;
 	}
 
 	inline uint16_t getValue(unsigned i = 0) const
 	{
-		return fRawPulse[i];
+		return fChannel[i].fRawPulse;
 	}
 
 	inline bool hasChanged(unsigned i = 0)
 	{
-		return abs(int32_t(fPulse[i]) - int32_t(fRawPulse[i])) > 20;
+		return abs(int32_t(fChannel[i].fPulse) - int32_t(fChannel[i].fRawPulse)) > 20;
 	}
 
 	inline unsigned numChannels() const
@@ -62,78 +61,133 @@ public:
 
 	unsigned long getAge(unsigned i = 0)
 	{
-		return millis() - fLastActive[i];
+		return millis() - fChannel[i].fLastActive;
 	}
 
 	bool isActive(unsigned i = 0)
 	{
-		return millis() > 500 && (fPulse[i] != 0 && getAge(i) < 500);
+		return millis() > 500 && (fChannel[i].fPulse != 0 && getAge(i) < 500);
 	}
 
 	bool becameActive(unsigned i = 0)
 	{
-		return (fAliveStateChange[i] && fAlive[i]);
+		return (fChannel[i].fAliveStateChange && fChannel[i].fAlive);
 	}
 
 	bool becameInactive(unsigned i = 0)
 	{
-		return (fAliveStateChange[i] && !fAlive[i]);
-	}
-
-	virtual void setup() override
-	{
-		begin();
+		return (fChannel[i].fAliveStateChange && !fChannel[i].fAlive);
 	}
 
 	virtual void animate() override
 	{
 		for (unsigned i = 0; i < fNumChannels; i++)
 		{
+		#ifndef RMT_RX_MODE
 			checkActive(i);
+		#endif
 			if (hasChanged(i))
 			{
-				fPulse[i] = fRawPulse[i];
+				fChannel[i].fPulse = fChannel[i].fRawPulse;
 				if (fChangeNotify != nullptr)
-					fChangeNotify(fGPIO[i], fPulse[i]);
+					fChangeNotify(fChannel[i].fGPIO, fChannel[i].fPulse);
 			}
-			fAliveStateChange[i] = false;
+			fChannel[i].fAliveStateChange = false;
 			if (isActive(i))
 			{
-				if (!fAlive[i])
+				if (!fChannel[i].fAlive)
 				{
-					DEBUG_PRINT("PWM Start Pin`: "); DEBUG_PRINTLN(fGPIO[i]);
-					fAlive[i] = true;
-					fAliveStateChange[i] = true;
+					DEBUG_PRINT("PWM Start Pin: "); DEBUG_PRINTLN(fChannel[i].fGPIO);
+					fChannel[i].fAlive = true;
+					fChannel[i].fAliveStateChange = true;
 				}
 			}
-			else if (fAlive[i])
+			else if (fChannel[i].fAlive)
 			{
-				DEBUG_PRINT("PWM End Pin: "); DEBUG_PRINTLN(fGPIO[i]);
-				fAlive[i] = false;
-				fAliveStateChange[i] = true;
+				DEBUG_PRINT("PWM End Pin: "); DEBUG_PRINTLN(fChannel[i].fGPIO);
+				fChannel[i].fAlive = false;
+				fChannel[i].fAliveStateChange = true;
 			}
 		}
 	}
 
+#ifdef RMT_RX_MODE
+	bool fStarted = false;
+	void begin()
+	{
+		if (fStarted)
+			return;
+		for (unsigned i = 0; i < fNumChannels; i++)
+		{
+		    if ((fChannel[i].fReceiver = rmtInit(fChannel[i].fGPIO, RMT_RX_MODE, RMT_MEM_64)) != NULL)
+		    {
+				rmtSetTick(fChannel[i].fReceiver, 1000);
+				rmtSetFilter(fChannel[i].fReceiver, true, 100);
+			    rmtSetRxThreshold(fChannel[i].fReceiver, 5000);
+			    rmtRead(fChannel[i].fReceiver, receive_data, &fChannel[i]);
+		    }
+		    else
+		    {
+		    	DEBUG_PRINT("FAILED TO INIT RMT on ");
+		    	DEBUG_PRINTLN(fChannel[i].fGPIO);
+		    }
+		}
+		fStarted = true;
+	}
+
+	void end()
+	{
+		if (!fStarted)
+			return;
+		for (unsigned i = 0; i < fNumChannels; i++)
+		{
+			if (fChannel[i].fReceiver != nullptr)
+			{
+				rmtEnd(fChannel[i].fReceiver);
+				rmtDeinit(fChannel[i].fReceiver);
+				fChannel[i].fReceiver = nullptr;
+			}
+		}
+		fStarted = false;
+	}
+#else
 	void begin();
 	void end();
+#endif
 
 private:
 	uint8_t fNumChannels = 1;
 	static constexpr unsigned fMaxChannels = 8;
-	uint8_t fChannels[fMaxChannels] = {};
-	uint8_t fGPIO[fMaxChannels] = {};
-	uint16_t fPulse[fMaxChannels] = {};
-	bool fAlive[fMaxChannels] = {};
-	bool fAliveStateChange[fMaxChannels] = {};
-	uint32_t fLastActive[fMaxChannels] = {};
-	rmt_isr_handle_t fISRHandle = nullptr;
+	struct Channel
+	{
+		uint8_t fGPIO;
+		uint16_t fPulse;
+		volatile uint16_t fRawPulse;
+		bool fAlive;
+		bool fAliveStateChange;
+		uint32_t fLastActive;
+		rmt_obj_t* fReceiver;
+	} fChannel[fMaxChannels] = {};
 	void (*fChangeNotify)(int pin, uint16_t pwm) = nullptr;
-	volatile uint16_t fRawPulse[fMaxChannels] = {};
-	static PWMDecoder* sActive;
 
+#ifdef RMT_RX_MODE
+	static void receive_data(uint32_t* data, size_t len, void* arg)
+	{
+		Channel* channel = (Channel*)arg;
+		rmt_data_t* it = (rmt_data_t*)data;
+		if (len >= 1)
+		{
+			channel->fRawPulse = it[0].duration0;
+			channel->fLastActive = millis();
+		}
+	}
+#else
+	rmt_isr_handle_t fISRHandle = nullptr;
 	static void IRAM_ATTR rmt_isr_handler(void* arg);
-	void checkActive(unsigned i = 0);
+	void checkActive(unsigned i);
+#endif
 };
+
+#define ServoDecoder PWMDecoder
 
 #endif
