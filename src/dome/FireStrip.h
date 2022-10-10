c2b7a1237b55c1774faf06f7ecd7f3cf6cd39392
rimim <nomail@thanks.com>
2022-10-10 07:25:17 -0400

    Added FastLED compatibility

--- src/dome/FireStrip.h
+++ src/dome/FireStrip.h
@@ -1,11 +1,33 @@
 #ifndef FireStrip_h
 #define FireStrip_h
 
-#include <Adafruit_NeoPixel.h>
 #include "ReelTwo.h"
+#include "core/LEDPixelEngine.h"
 #include "core/SetupEvent.h"
 #include "core/AnimatedEvent.h"
 #include "core/CommandEvent.h"
+
+#if USE_LEDLIB == 0
+template<uint8_t DATA_PIN, uint32_t RGB_ORDER, uint16_t NUM_LEDS>
+class FireStripPCB : public FastLED_NeoPixel<NUM_LEDS, DATA_PIN, RGB_ORDER>
+{
+public:
+    FireStripPCB() {}
+};
+#define USE_FIRESTRIP_TEMPLATE 1
+#elif USE_LEDLIB == 1
+template<uint8_t DATA_PIN, uint32_t RGB_ORDER, uint16_t NUM_LEDS>
+class FireStripPCB : public Adafruit_NeoPixel
+{
+public:
+    FireStripPCB() :
+        Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, RGB_ORDER)
+    {
+    }
+};
+#else
+ #error Unsupported
+#endif
 
 /**
   * \ingroup Dome
@@ -23,11 +45,24 @@
   *  fireStrip.burn(500);
   * \endcode
   */
+#if USE_FIRESTRIP_TEMPLATE
+template<uint8_t DATA_PIN, uint32_t RGB_ORDER = GRB, uint16_t NUM_LEDS = 8>
+class FireStrip :
+    private FireStripPCB<DATA_PIN, RGB_ORDER, NUM_LEDS>,
+    public SetupEvent, AnimatedEvent, CommandEvent
+#else
 class FireStrip :
     private Adafruit_NeoPixel,
     public SetupEvent, AnimatedEvent, CommandEvent
+#endif
 {
 public:
+#if USE_FIRESTRIP_TEMPLATE
+    FireStrip(const int id = 0) :
+        fID(id)
+    {
+    }
+#else
     enum PixelType
     {
         kRGBW = NEO_GRBW + NEO_KHZ800,
@@ -39,6 +74,7 @@ public:
         fID(id)
     {
     }
+#endif
 
     /**
       * \returns FireStrip Jawa ID
@@ -76,7 +112,7 @@ public:
         fEffectEnd = 0;
         fFlipFlop = false;
         fEffectType = kNoEffect;
-        for (unsigned i = 0; i < numLEDs; i++)
+        for (unsigned i = 0; i < NUM_LEDS; i++)
             setPixelColor(i, 0);
         dirty();
     }
@@ -172,7 +208,29 @@ public:
     }
 
 private:
+#if USE_FIRESTRIP_TEMPLATE
+    void begin()
+    {
+        FireStripPCB<DATA_PIN, RGB_ORDER, NUM_LEDS>::begin();
+    }
+
+    void show()
+    {
+        FireStripPCB<DATA_PIN, RGB_ORDER, NUM_LEDS>::show();
+    }
+
+    void setPixelColor(uint16_t n, uint32_t c)
+    {
+        FireStripPCB<DATA_PIN, RGB_ORDER, NUM_LEDS>::setPixelColor(n, c);
+    }
+
+    void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)
+    {
+        FireStripPCB<DATA_PIN, RGB_ORDER, NUM_LEDS>::setPixelColor(n, r, g, b);
+    }
+#else
     constexpr static int NUM_LEDS = 8;
+#endif
     enum
     {
         BRIGHT = 100
@@ -202,9 +260,9 @@ private:
 
     void setAll(byte red, byte green, byte blue)
     {
-        for (unsigned i = 0; i < numLEDs; i++)
+        for (unsigned i = 0; i < NUM_LEDS; i++)
         {
-            setPixelColor(i, Color(red, green, blue));
+            setPixelColor(i, red, green, blue);
         }
         dirty();
     }
@@ -264,17 +322,17 @@ private:
         if (t192 > 0x80)
         {
             // hottest
-            setPixelColor(pixel, Color(255, 255, heatramp));
+            setPixelColor(pixel, 255, 255, heatramp);
         }
         else if (t192 > 0x40)
         {
             // middle
-            setPixelColor(pixel, Color(255, heatramp, 0));
+            setPixelColor(pixel, 255, heatramp, 0);
         }
         else
         {
             // coolest
-            setPixelColor(pixel, Color(heatramp, 0, 0));
+            setPixelColor(pixel, heatramp, 0, 0);
         }
     }
 };
